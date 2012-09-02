707df45580262f9370a0dcd874e88011d2643f85
Alexander Grabanski <spagrab@gmail.com>
2012-09-02 15:36:53 -0500

    Completed "client" [Robot-side] comm code.
    All that's left to do is to actually test it.

--- Robot/Misc/communications.h
+++ Robot/Misc/communications.h
@@ -4,6 +4,7 @@ Contributors:
 Alex Grabanski */
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <vectormath.h>
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xD5, 0x13 };
 IPAddress ip(192, 168, 1, 178);
@@ -12,7 +13,7 @@ unsigned int localPort = 8888;
 
 typedef union {
     char data[UDP_TX_PACKET_MAX_SIZE]; 
-    scalar_t joystickaxes[4]; //counting on the above being larger
+    char joystickaxes[4]; //counting on the above being larger
 } packet;
 
 EthernetUDP Udp;
@@ -23,7 +24,15 @@ void netSetup() {
 }
 packet readPacket() {
     packet result;
-    Udp.read(result.data,UDP_TX_PACKET_MAX_SIZE);
+    Udp.read(&result.data,UDP_TX_PACKET_MAX_SIZE);
     return result;
 }
-
+scalar_t toAxis(char b) {
+    return (b / 127.0);
+}
+vector2d getLeftJoy(packet p) {
+    return vec(toAxis(p[0]), toAxis(p[1]));
+}
+vector2d getRightJoy(packet p) {
+    return vec(toAxis(p[2]), toAxis(p[3]));
+}
--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -3,14 +3,20 @@
 #include <SPI.h>
 #include <mecanumdrive.h>
 #include <drive.h>
+#include <communications.h>
 Servo motors[4];
 void setup() {
     motors[0].attach(5);
     motors[1].attach(6);
     motors[2].attach(7);
     motors[3].attach(8);
+    netSetup();
 }
 void loop() {
+   /*
+   packet in = readPacket();
+   drivetrain test = simpleMecanum(getLeftJoy(in), getRightJoy(in).x, &joyScaleAbs);
+   */
    drivetrain test =  simpleMecanum(vec(0.5,0.0), 0.0, &joyScaleAbs);
    simpleServoDrive(drivetrainToServo(test), motors);
 }
